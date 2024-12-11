7ccdd1aa5aaa337a5a5c6215681a6e2649c78141
Chingapo <arjunpareek03@gmail.com>
2024-12-11 22:28:40 +0530

    production fix

--- app/firebaseAdmin.ts
+++ app/firebaseAdmin.ts
@@ -1,12 +1,11 @@
 import admin from 'firebase-admin';
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: privateKey,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
